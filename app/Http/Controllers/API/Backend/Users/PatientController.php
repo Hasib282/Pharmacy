664a8ed88d0633f783fbf564e06790f0ffdabe4c
hasib282 <hasiburrahman81098@gmail.com>
2025-04-15 16:11:55 +0600

    User/Patient update

--- app/Http/Controllers/API/Backend/Users/PatientController.php
+++ app/Http/Controllers/API/Backend/Users/PatientController.php
@@ -11,7 +11,7 @@ use App\Models\Patient_Information;
 class PatientController extends Controller
 {
     public function ShowAll(Request $req){
-        $data = Patient_Registration::on('mysql_second')->paginate(15);//with is used to bring data from the doctors table ->with('doctors')
+        $data = Patient_Information::on('mysql_second')->paginate(15);//with is used to bring data from the doctors table ->with('doctors')
 
         return response()->json([
             'data'=> $data,
@@ -20,131 +20,11 @@ class PatientController extends Controller
         ],200);
     }
 
-    //Insert
-
-    public function Insert(Request $req){
-        
-        if($req->patient_type == "new"){
-            //$reg_id = 'R00000000001';
-            //$ptn_id = 'P00000000001';
-
-            //Patient id auto generate
-            $patient_id = Patient_Information::on('mysql_second')->select('ptn_id')->orderby('ptn_id','desc')->first();
-            if($patient_id){
-                //p001
-                $newptn_id = intval(substr($patient_id->ptn_id,1));
-                $newptn_id++;
-
-            }
-            else{
-                $newptn_id = 1;
-            }
-            $ptn_id= "P".str_pad($newptn_id,11,'0',STR_PAD_LEFT);
-
-            
-
-            //Patient id auto generation ends
-
-
-
-            //validation
-
-            $req->validate([
-                'title'=>'required',
-                'name'=> 'required',
-                'address'=> 'required',
-                'phone'=> 'required',
-                'email'=> 'required',
-                
-                'gender'=> 'required',
-                'nationality'=> 'required',
-                'religion'=> 'required',
-                'doctor'=> 'required',
-                
-
-            ]);
-
-
-            //converting age to  string to make a singe data
-
-            $age_year = $req->age_years;
-            $age_month = $req->age_months;
-            $age_day = $req->age_days;
-            //concatinate
-            $age =$age_year . " years, " . $age_month . " months, " . $age_day . " days";
-            
-
-            Patient_Information::on('mysql_second')->insert([
-                'ptn_id'=>$ptn_id,
-                'title'=>$req->title,
-                'name'=> $req->name,
-                'address'=> $req->address,
-                'phone'=> $req->phone,
-                'email'=> $req->email,
-                'age'=>$age,
-                'gender'=> $req->gender,
-                'nationality'=> $req->nationality,
-                'religion'=> $req->religion,
-                'doctor'=> $req->doctor,
-
-            ]);
-
-            return response()->json([
-                'status'=> true,
-                'message'=> 'added sucessfully'
-            ]);
-
-        }
-        else{
-            //auto generate reg id
-            $reg_id = Patient_Registration::on('mysql_second')->select('reg_id')->where('ptn_id',$req->pid)->orderby('reg_id','desc')->first();
-            //$reg_no = 1;
-            if($reg_id){
-                //p001
-                $newreg_id = intval(substr($reg_id->reg_id,1));
-                $newreg_id++;
-
-            }
-            else{
-                $newreg_id = 1;
-            }
-
-            $reg_id= R.str_pad($newreg_id,11,'0',STR_PAD_LEFT);
-
-            //auto generate reg id ends
-
-            $req-> validate([
-            
-                'bed_catagory'=>'required',
-                'bed_list'=>'required',
-                'doctor'=> 'required',//|exists:mysql_second.doctor__information.id',
-                'sr_id'=> 'required'
-
-            ]);
-            
-            Patient_Registration::on('mysql_second')->insert([
-                'reg_id'=>$reg_id,
-                'ptn_id'=>$req->pid,
-                'bed_category'=>$req->bed_category,
-                'bed_list'=> $req->bed_list,
-                'doctor'=> $req->doctor,
-                'sr_id'=> $req->sr_id
-            ]);
-        }
-        
-
-        return response()->json([
-            'status'=> true,
-            'message'=> 'added sucessfully'
-
-        ]);
-
     
-    }
 
     //Edit 
     public function Edit(Request $req){
-        $data = Patient_Registration::on('mysql_second')->findOrFail($req-> id);
+        $data = Patient_Information::on('mysql_second')->findOrFail($req-> id);
 
         return response()->json([
             'status'=> true,
@@ -155,31 +35,31 @@ class PatientController extends Controller
     //Update
     public function Update(Request $req){
         $req->validate([
-            'pid'=> 'required',
-            'rid'=>'required',
+            //'pid'=> 'required',
+            //'rid'=>'required',
             'title'=>'required',
             'name'=> 'required',
             'address'=> 'required',
-            'age'=> 'required',
+            //'age'=> 'required',
             'gender'=> 'required',
             'nationality'=> 'required',
             'religion'=> 'required',
-            'doctor'=> 'required',
-            'sr'=> 'required'
+            //'doctor'=> 'required',
+            //'sr'=> 'required'
         ]);
 
-        $update = Patient_Registration::on('mysql_second')->findOrFail($req-> id)->update([
-            'pid'=> $req->pid,
-            'rid'=>$req->rid,
+        $update = Patient_Information::on('mysql_second')->findOrFail($req-> id)->update([
+            //'pid'=> $req->pid,
+            //'rid'=>$req->rid,
             'title'=>$req->title,
             'name'=> $req->name,
             'address'=> $req->address,
-            'age'=> $req->age,
+            // 'age'=> $req->age,
             'gender'=> $req->gender,
             'nationality'=> $req->nationality,
             'religion'=> $req->religion,
-            'doctor'=> $req->doctor,
-            'sr'=> $req->sr,
+            //'doctor'=> $req->doctor,
+            //'sr'=> $req->sr,
             "updated_at" => now()
         ]);
 
@@ -195,7 +75,7 @@ class PatientController extends Controller
 
     //delete
     public function Delete(Request $req){
-        Patient_Registration::on('mysql_second')->findOrFail($req->id)->delete();
+        Patient_Information::on('mysql_second')->findOrFail($req->id)->delete();
 
         return response()->json([
             'status'=> true,
--- public/js/ajax/hospital/users/patient.js
+++ public/js/ajax/hospital/users/patient.js
@@ -1,3 +1,4 @@
+//Show patient start
 function ShowPatients(data, startIndex) {
     let tableRows = '';
     
@@ -36,6 +37,9 @@ function ShowPatients(data, startIndex) {
     }
 }; // End Function
 
+//Show patient end
+
+
 
 
 $(document).ready(function () {
@@ -59,6 +63,46 @@ $(document).ready(function () {
 
     // Update Ajax
     UpdateAjax('hospital/users/patients', ShowPatients);
+
+    
+     // Update Ajuax
+     $(document).on('submit','#EditForm', function (e) {
+         e.preventDefault();
+//         let formData = new FormData(this);
+
+        $.ajax({
+            url: "/api/hospital/setup/patients",
+            method: 'POST',
+            processData: false,
+            contentType: false,
+            cache: false,
+            data: formData,
+            beforeSend:function() {
+                $(document).find('span.error').text('');  
+            },
+            success: function (res) {
+                console.log(res);
+                
+                if (res.status) {
+                    $('#editModal').hide();
+                    $('#EditForm')[0].reset();
+                    ShowPatients();
+                    // $('.table').load(location.href + ' .table');
+                }
+            },
+
+            error: function (err) {
+                console.log(err);
+
+                let error = err.responseJSON;
+                $.each(error.errors, function (key, value) {
+                    $('#update_' + key + "_error").text(value);
+                });
+            }
+        })
+
+
+    });
     
 
     // Delete Ajax
@@ -77,7 +121,20 @@ $(document).ready(function () {
     function EditFormInputValue(res){
         $('#id').val(res.data.id);
         $('#updateName').val(res.data.name);
-        $('#updateFloor').val(res.data.floor);
+        $('#updateTitle').val(res.data.title);
+        $('#updatePhone').val(res.data.phone);
+        $('#updateAddress').val(res.data.address);
+        $('#updateEmail').val(res.data.email);
+
+        $('#updateAge_years').val(res.data.age_years);
+        $('#updateAge_months').val(res.data.age_months);
+        $('#updateAge_days').val(res.data.age_days);
+        
+        $('#updateGender').val(res.data.gender);
+        $('#updateNationality').val(res.data.nationality);
+        $('#updateReligion').val(res.data.religion);
+        
+
         $('#updateTitle').focus();
     }; // End Method
 });
