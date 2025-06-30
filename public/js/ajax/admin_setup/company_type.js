d10753e34f73d435d53c5bec80dab2dd83db291a
HASIBUR RAHAMAN <60450950+Hasib282@users.noreply.github.com>
2025-06-30 14:46:51 +0600

    Delete Status Button add

--- public/js/ajax/admin_setup/company_type.js
+++ public/js/ajax/admin_setup/company_type.js
@@ -6,6 +6,8 @@ function ShowCompanyTypes(res) {
         actions: (row) => `
                 <button data-modal-id="editModal" id="edit" data-id="${row.id}"><i class="fas fa-edit"></i></button>
                         
+                <button data-id="${row.id}" id="delete_status"><i class="fa-solid fa-trash-arrow-up"></i></button>
+
                 <button data-id="${row.id}" id="delete"><i class="fas fa-trash"></i></button>
                 `,
     });
