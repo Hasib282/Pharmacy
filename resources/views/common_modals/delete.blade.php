707f3b2b4cbd7104c24f40cf204c31163c955067
hasib282 <hasiburrahman81098@gmail.com>
2025-06-30 16:02:49 +0600

    Delete Status Modal Created

--- resources/views/common_modals/delete.blade.php
+++ resources/views/common_modals/delete.blade.php
@@ -5,7 +5,7 @@
             <h3 class="center">Are you sure?</h3>
             <span class="close-modal" data-modal-id="deleteModal" style="top: 10px;">&times;</span>
         </div>
-        <p class="center">Are you sure you want to delete this {{ $name }}?</p>
+        <p class="center">Are you sure you want to Permanently delete this {{ $name }}?</p>
         <div class="center button">
             <button type="button" class="btn-blue"  id="confirm">Yes</button>
             <button type="button" class="btn-red" id="cancel" data-dismiss="modal">No</button>
